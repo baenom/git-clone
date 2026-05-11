cc11ee907b0dd11f470881d7575441e1ddb90834
EuijinBae <baenom118@gmail.com>
2026-05-11 11:45:31 +0900

    26/05/11

---
+++ Python26/Bank/console_bank.py
@@ -0,0 +1,67 @@
+class Account:
+    def __init__(self,account_no,owner,blance):
+        self.__account_no = account_no
+        self.__owner = owner
+        self.__blance = blance
+    def __str__(self):
+        return f"{self.__account_no}\t{self.__owner}\t{self.__blance}"
+    def deposit(self,amount):
+        self.blance += amount
+    def withdrow(self,amount):
+        self.blance += amount
+    def get_account_no(self):
+        return self.__account_no
+    def get_owner(self):
+        return self.__owner
+    def get_blance(self):
+        return self.__blance
+class AccountService:
+    def __init__(self):
+        self.__account_list = []
+    def create_account(self, account_no, owner, balance):
+        account = Account(account_no, owner, balance)
+        self.__account_list.append(account)
+    def get_list_account(self):
+        return self.__account_list
+    def deposit(self,account_no,amount):
+        for account in self.__account_list:
+            if account.get_account_no() == account_no:
+                account.deposit(amount)
+                break
+
+
+def select_manu():
+    print("===============================================")
+    print('1.계좌생성 | 2.계좌목록 | 3.입금 | 4.출금 | 0.종료')
+    print("===============================================")
+    menu = int(input('>> 메뉴선택 : '))
+    return menu
+
+aservice = AccountService()
+print()
+print('=================Euijin Bank==================')
+while True:
+    menu = select_manu()
+    if menu == 0: break
+    elif menu == 1:
+        account_no = input("> 계좌번호 : ")
+        owner = input("> 계좌주 : ")
+        balance = int(input("> 초기 입금액 : "))
+        if aservice.create_account(account_no,owner,balance):
+            print('계좌가 생성되었습니다')
+    elif menu == 2:
+        list_account = aservice.get_list_account()
+        print('=============')
+        print('===계좌목록===')
+        print('=============')
+        for account in list_account:
+            print(account)
+    elif menu == 3:
+        print('=============')
+        print('=====예금====')
+        print('=============')
+        account_no = input('> 계좌번호')
+        amount = int(input('> 예금액 : '))
+        aservice.deposit(account_no, amount)
+
+print('=============이용해주셔서 감사합니다============')
