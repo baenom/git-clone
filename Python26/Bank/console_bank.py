3a678cc770aebed55a2d25f73ca4d5581d0b924c
EuijinBae <baenom118@gmail.com>
2026-05-11 11:57:14 +0900

    26/05/11

--- Python26/Bank/console_bank.py
+++ Python26/Bank/console_bank.py
@@ -6,9 +6,9 @@ class Account:
     def __str__(self):
         return f"{self.__account_no}\t{self.__owner}\t{self.__blance}"
     def deposit(self,amount):
-        self.blance += amount
+        self.__blance += amount
     def withdrow(self,amount):
-        self.blance += amount
+        self.__blance += amount
     def get_account_no(self):
         return self.__account_no
     def get_owner(self):
@@ -28,6 +28,12 @@ class AccountService:
             if account.get_account_no() == account_no:
                 account.deposit(amount)
                 break
+    def withdrow(self,account_no,amount):
+        for account in self.__account_list:
+            if account.get_account_no() == account_no:
+                account.withdrow(amount)
+                break
+        
 
 
 def select_manu():
@@ -63,5 +69,14 @@ while True:
         account_no = input('> 계좌번호')
         amount = int(input('> 예금액 : '))
         aservice.deposit(account_no, amount)
+        print(f"{amount}원이 입금되었습니다")
+    elif menu == 4:
+        print('=============')
+        print('=====출금====')
+        print('=============')
+        account_no = input('> 계좌번호')
+        amount = int(input('> 출금액 : '))
+        aservice.deposit(account_no, amount)
+        print(f"{amount}원이 출금되었습니다")
 
 print('=============이용해주셔서 감사합니다============')
